1e170e1fbb373c759b3e45b7a87b37cb816bb6a7
Elcazar <alejandroalcazar09@gmail.com>
2025-02-25 08:44:38 +0100

    test_pasados

--- src/Lab_2_4_LR2.py
+++ src/Lab_2_4_LR2.py
@@ -65,10 +65,10 @@ class LinearRegressor:
             None: Modifies the model's coefficients and intercept in-place.
         """
         # Replace this code with the code you did in the previous laboratory session
-
+        w = np.linalg.inv((np.transpose(X)@X))@(np.transpose(X)@y)
         # Store the intercept and the coefficients of the model
-        self.intercept = None
-        self.coefficients = None
+        self.intercept = w[0]
+        self.coefficients = w[1:]
 
     def fit_gradient_descent(self, X, y, learning_rate=0.01, iterations=1000):
         """
@@ -91,19 +91,18 @@ class LinearRegressor:
         )  # Small random numbers
         self.intercept = np.random.rand() * 0.01
 
-        # Implement gradient descent (TODO)
+        # Implement gradient descent 
         for epoch in range(iterations):
-            predictions = None
+            predictions = self.predict(X[:,1])
             error = predictions - y
+            
+            # Write the gradient values and the updates for the paramenters
+            gradient = (learning_rate/len(X))*np.dot(X.T, error)
+            self.intercept -= gradient[0]
+            self.coefficients -= gradient[1]
 
-            # TODO: Write the gradient values and the updates for the paramenters
-            gradient = None
-            self.intercept -= None
-            self.coefficients -= None
-
-            # TODO: Calculate and print the loss every 10 epochs
             if epoch % 1000 == 0:
-                mse = None
+                mse = np.sum(np.power((error),2))/len(y)
                 print(f"Epoch {epoch}: MSE = {mse}")
 
     def predict(self, X):
@@ -121,13 +120,14 @@ class LinearRegressor:
             ValueError: If the model is not yet fitted.
         """
 
-        # Paste your code from last week
-
         if self.coefficients is None or self.intercept is None:
             raise ValueError("Model is not yet fitted")
 
-        return None
-
+        if np.ndim(X) == 1:
+            predictions = self.intercept + self.coefficients*X
+        else:
+            predictions = X@self.coefficients + self.intercept
+        return predictions
 
 def evaluate_regression(y_true, y_pred):
     """
@@ -142,16 +142,15 @@ def evaluate_regression(y_true, y_pred):
     """
 
     # R^2 Score
-    # TODO
-    r_squared = None
+    RSS = np.sum(np.power((y_true-y_pred),2))
+    TSS = np.sum(np.power((y_true-np.mean(y_true)),2))
+    r_squared = 1 - (RSS/TSS)
 
     # Root Mean Squared Error
-    # TODO
-    rmse = None
+    rmse = np.sqrt(np.sum(np.power((y_true-y_pred),2))/len(y_true))
 
     # Mean Absolute Error
-    # TODO
-    mae = None
+    mae = np.sum(np.abs(y_true-y_pred))/len(y_true)
 
     return {"R2": r_squared, "RMSE": rmse, "MAE": mae}
 
@@ -172,19 +171,24 @@ def one_hot_encode(X, categorical_indices, drop_first=False):
     X_transformed = X.copy()
     for index in sorted(categorical_indices, reverse=True):
         # TODO: Extract the categorical column
-        categorical_column = None
+        categorical_column = X[:, index]
 
         # TODO: Find the unique categories (works with strings)
-        unique_values = None
+        unique_values = np.unique(categorical_column)
 
         # TODO: Create a one-hot encoded matrix (np.array) for the current categorical column
-        one_hot = None
+        one_hot = np.array(
+            [
+                [1 if category == val else 0 for val in unique_values] for category in categorical_column
+            ]
+        )
 
         # Optionally drop the first level of one-hot encoding
         if drop_first:
             one_hot = one_hot[:, 1:]
 
         # TODO: Delete the original categorical column from X_transformed and insert new one-hot encoded columns
-        X_transformed = None
+        X_transformed = np.delete(X_transformed, index, axis=1)
+        X_transformed = np.insert(X_transformed, index, one_hot.T, axis=1)
 
     return X_transformed
